f43dc8497b33d7a7fd765d41cb7e30ba9bddd73a
Ajaykumar-N-GITHUB <ajayjothika17@gmail.com>
2024-10-01 15:44:46 +0530

    latest code for branch-3

--- Program-1-variables/hello.go
+++ Program-1-variables/hello.go
@@ -6,12 +6,12 @@ func main() {
 	var name = "Ajay"
 	var age int = 25
 
-	fmt.Printf("This is %s and i am %d years old. \t The type of parameter used in this Print function are %T and %T", name, age, name, age)
+	fmt.Printf("This is %s and i am %d years old. \t The type of parameter used in this Print function are %T and %T\n", name, age, name, age)
 
-	a := 1
-	fmt.Println(a)
-	a = 8
-	fmt.Println(a)
+	a := 112
+	fmt.Println(a / 10)
+	b := 845
+	fmt.Println(b % 10)
 
 	b, c, _, e := 1, 2.0, 3, "Numbers"
 	fmt.Println(b, c, e)
